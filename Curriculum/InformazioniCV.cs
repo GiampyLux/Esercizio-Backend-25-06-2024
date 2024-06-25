68b0415ae1cf63e8487d018faa2876d2c20f10da
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-25 09:46:53 +0200

    Aggiungere i file di progetto.

---
+++ Curriculum/InformazioniCV.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curriculum
+{
+    internal class InformazioniCV
+    {
+        public class Informazioni
+        {
+            public string Nome { get; set; }
+            public string Cognome { get; set; }
+            public string Telefono { get; set; }
+            public string Email { get; set; }
+
+
+        }
+
+        public class Studi
+        {
+            public string Qualifica { get; set; }
+            public string Istituto { get; set; }
+            public string Tipo { get; set; }
+            public DateTime Dal { get; set; }
+            public DateTime Al { get; set; }
+
+        }
+
+        public class Impiego
+        {
+            public Esperienza Esperienza { get; set; }
+        }
+
+        public class Esperienza
+        {
+            public string Azienda { get; set; }
+            public string TitoloLavoro { get; set; }
+            public DateTime Dal { get; set; }
+            public DateTime Al { get; set; }
+            public string Descrizione { get; set; }
+            public string Compiti
+            {
+                get; set;
+            }
+        }
+
+        public class CV
+        {
+            public Informazioni Informazioni { get; set; }
+            public List<Studi> StudiEffettuati { get; set; }
+            public List<Impiego> Impieghi { get; set; }
+            public CV()
+            {
+                StudiEffettuati = new List<Studi>();
+                Impieghi = new List<Impiego>();
+            }
+        }
+
+       
+    }
+}
---
+++ Curriculum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using static Curriculum.InformazioniCV;
+
+namespace Curriculum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InformazioniCV.CV MioCv = new InformazioniCV.CV();
+            MioCv.Informazioni = new InformazioniCV.Informazioni()
+            {
+                Nome = "Giampaolo",
+                Cognome = "Paladino",
+                Telefono = "3472582545",
+                Email = "example@gmail.com",
+            };
+
+            MioCv.StudiEffettuati.Add(new InformazioniCV.Studi
+            {
+                Qualifica = "Epicode Student",
+                Istituto = "Epicode Sede Milano",
+                Tipo = "Universitario",
+                Dal = new DateTime(2024, 3, 1),
+                Al = new DateTime(2024, 9, 20),
+            });
+
+            MioCv.Impieghi.Add(new InformazioniCV.Impiego
+            {
+                Esperienza = new InformazioniCV.Esperienza
+                {
+                    Azienda = "Tim SPA",
+                    TitoloLavoro = "Sviluppatore",
+                    Dal = new DateTime(2025, 3, 4),
+                    Al = DateTime.Now,
+                    Descrizione = "programmatore del sito web",
+                    Compiti = "Scrivere tutto il codice",
+                }
+            });
+            Stampa(MioCv);
+
+            static void Stampa(InformazioniCV.CV cv)
+            {
+                Console.WriteLine("=== CURRICULUM VITAE ===");
+                Console.WriteLine("Informazioni Personali:");
+                Console.WriteLine($"Nome: {cv.Informazioni.Nome}");
+                Console.WriteLine($"Cognome: {cv.Informazioni.Cognome}");
+                Console.WriteLine($"Telefono: {cv.Informazioni.Telefono}");
+                Console.WriteLine($"Email: {cv.Informazioni.Email}");
+                Console.WriteLine();
+
+                Console.WriteLine("Studi Effettuati");
+                foreach (var studi in cv.StudiEffettuati)
+                {
+                    Console.WriteLine($"Qualifica: {studi.Qualifica}");
+                    Console.WriteLine($"Istituto: {studi.Istituto}");
+                    Console.WriteLine($"Tipo: {studi.Tipo}");
+                    Console.WriteLine($"Dal: {studi.Dal.ToShortDateString()}");
+                    Console.WriteLine($"Al: {studi.Al.ToShortDateString()}");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Impieghi");
+                foreach (var impiego in cv.Impieghi)
+                {
+                    Console.WriteLine($"Azienda: {impiego.Esperienza.Azienda}");
+                    Console.WriteLine($"Titolo Lavoro: {impiego.Esperienza.TitoloLavoro}");
+                    Console.WriteLine($"Dal: {impiego.Esperienza.Dal.ToShortDateString()}");
+                    Console.WriteLine($"Al: {impiego.Esperienza.Al.ToShortDateString()}");
+                    Console.WriteLine($"Descrizione: {impiego.Esperienza.Descrizione}");
+                    Console.WriteLine($"Compiti: {impiego.Esperienza.Compiti}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
